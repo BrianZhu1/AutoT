915db353539fe69549985404d2b359ae92c05cf1
smerdy <joshua.pham@gmail.com>
2016-04-16 16:51:53 -0500

    debugging itty basic stuff

--- application.py
+++ application.py
@@ -1,11 +1,3 @@
-from itty import *
-from tropo import Tropo
-
 @get('/')
 def index(request):
-
-    t = Tropo()
-    t.say("Welcome to Tropo!")
-    return t.RenderJson()
-
-run_itty(server='wsgiref', host='0.0.0.0', port=8888)
+    return 'Hello World!'
