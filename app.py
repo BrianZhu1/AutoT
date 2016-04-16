e813b871d34dd3bf175ce9883090dbb2c0fce283
smerdy <joshua.pham@gmail.com>
2016-04-16 18:39:55 -0500

    added tropo back

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 import os
 from flask import Flask
+from tropo import *
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	var callerID = currentCall.callerID;
+	# callerID = currentCall.callerID
 
-	say("Welcome to speed therapy!");
+	say("Welcome to speed therapy!")
 	record("Tell us how you feel in fifteen minutes or less!", {
-	    beep:false,
-	    maxTime:900,
-	    transcriptionOutURI: "http://autotapp.herokuapp.com/transcribed_audio",
-	    transcriptionOutFormat: 'json'
+	    "beep":False,
+	    "maxTime":900,
+	    "transcriptionOutURI": "http://autotapp.herokuapp.com/transcribed_audio",
+	    "transcriptionOutFormat": 'json'
 	    }
-	);
+	)
+
 	return 'Hello World!'
 
 @app.route("/transcribed_audio")
-def transcribed_audio:
+def transcribed_audio():
 	return request.json
 
 if __name__ == "__main__":
