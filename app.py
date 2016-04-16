bc9a96e3c0bcb59b2fa4e4029f2604c600cf6b17
smerdy <joshua.pham@gmail.com>
2016-04-16 18:30:07 -0500

    added transcribed_audio route

--- app.py
+++ app.py
@@ -4,7 +4,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python!"
+	var callerID = currentCall.callerID;
+
+	say("Welcome to speed therapy!");
+	record("Tell us how you feel in fifteen minutes or less!", {
+	    beep:false,
+	    maxTime:900,
+	    transcriptionOutURI: "http://autotapp.herokuapp.com/transcribed_audio",
+	    transcriptionOutFormat: 'json'
+	    }
+	);
+
+@app.route("/transcribed_audio")
+def transcribed_audio:
+	return request.json
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
