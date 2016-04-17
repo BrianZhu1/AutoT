07c4114df48cac99926f660476164d4f58f6ca58
smerdy <joshua.pham@gmail.com>
2016-04-17 06:10:02 -0500

    fixed keyerror 2

--- firebase_abstractions.py
+++ firebase_abstractions.py
@@ -6,7 +6,11 @@ baseURL = "https://autotapp.firebaseio.com/"
 # Helper functions to ease CRUD functions to DB.
 
 def fetchMenu(companyName):
-	return requests.get(baseURL + "menu.json").json()[companyName]
+	optionData = requests.get(baseURL + "menu.json").json()
+	if companyName in optionData:
+		return optionData[companyName]
+	else:
+		return None
 
 def fetchNumber(companyName):
 	optionData = requests.get(baseURL + "directory.json").json()
