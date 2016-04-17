3ffcb57b06bddf481263d9310154533b2287b152
smerdy <joshua.pham@gmail.com>
2016-04-17 04:37:23 -0500

    removed unnecessary files

--- fbtest.py
+++
@@ -1,5 +0,0 @@
-from firebase import firebase
-
-firebase = firebase.FirebaseApplication('https://autotapp.firebaseio.com', None)
-result = firebase.post('/users', 'asdfasdfasdf')
-print result
--- static/test.py
+++
@@ -1,8 +0,0 @@
-answer()
-
-result = ask( "Hello.  Please enter any number", { 'choices' : "[DIGITS]" })
-
-if result.name == 'choice' :
-    say( "Great, you said " + result.value )
-
-hangup()
