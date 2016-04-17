9b32246e98f8792d7d8af9654f777c737b2a2eea
smerdy <joshua.pham@gmail.com>
2016-04-17 06:17:35 -0500

    fixing app.py?

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def hello():
     
     S = Session(request.data)
     T = Tropo()
-    T.call(to="14084827871")
+    T.call(to="+14084827871")
     T.say("Welcome to speed therapy!")
     T.record(say="Tell us how you feel in fifteen minutes or less!", \
         beep=False, \
@@ -39,7 +39,7 @@ def scrape_menu(phone, s, t):
 @app.route("/transcribe", methods=["POST"])
 def transcribe():
     headers = {'Content-Type': 'application/json'}
-    requests.put('https://autotapp.firebaseio.com/results.json', data=json.dumps(request.data))
+    requests.put('https://autotapp.firebaseio.com/results.json', data=json.dumps(request.data[transcription]))
     return redirect('/')
 
 
