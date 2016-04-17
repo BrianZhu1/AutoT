38d41daadd3e4f54bfdba857eaa9dd64acdaf741
smerdy <joshua.pham@gmail.com>
2016-04-17 07:47:42 -0500

    removing partial code to verify transcribe 5

--- app.py
+++ app.py
@@ -22,48 +22,51 @@ def hello():
     
     S = Session(request.data)
     T = Tropo()
-    T.call(to="+14084827871")
-    T.say("Welcome to speed therapy!")
-    T.record(say="Tell us how you feel in fifteen minutes or less!", \
+    number = "+14084827871"
+    # T.call(to=number)
+    # T.say("Welcome to speed therapy!")
+    # T.record(say="Tell us how you feel in fifteen minutes or less!", \
+    #     beep=False, \
+    #     maxTime=8, \
+    #     transcription= {"url": "http://autotapp.herokuapp.com/transcribe"}, \
+    #     format='json'
+    #     )
+    traverse_menu(number, S, T)
+    return T.RenderJson()
+
+def traverse_menu(phone, S, T): #it will find the next level of dtmf tones
+        
+    option_list = ["1","3", "2"] #actual values of dtmf tones
+    quadruple_p = "p"*4 #four second delay between each dtmf tone
+
+    call_str = ";postd="
+
+    for i in range(3):
+        if i != 0:
+            call_str = call_str + quadruple_p
+        call_str = call_str + option_list[i]
+
+    call_str = call_str + ";pause=2000ms"
+
+    if len(option_list) == 0:
+        call_str = ""
+
+    T.call(to=phone + call_str) #call_str = ;postd=1pppp2;pause=2000ms
+
+    #pushes the data to the server
+    T.record(say="", \
         beep=False, \
-        maxTime=8, \
+        maxTime=10, \
         transcription= {"url": "http://autotapp.herokuapp.com/transcribe"}, \
         format='json'
         )
-    return T.RenderJson()
 
-# def scrape_menu(phone, S, T): #it will find the next level of dtmf tones
-        
-#     option_list = ["1","2", "3"] #actual values of dtmf tones
-#     quadruple_p = "pppp" #four second delay between each dtmf tone
+    # sleep(3000)
 
-#     call_str = ";postd="
+    # optionsCorpus = requests.get('https://autotapp.firebaseio.com/results.json').json()
+    # optionList = filter(None, [option.strip(" , or and").split()[1:] \
+    #     for option in optionsCorpus.lower().split("press")])
 
-#     for(i in range(0, len(option_list))):
-#         if(i != 0 ):
-#             call_str = call_str + quadruple_p
-#         call_str = call_str + option_list[i]
-
-#     call_str = call_str + ";pause=2000ms"
-
-#     if(len(option_list) == 0):
-#         call_str = ""
-
-#     T.call(to=phone + call_str) #call_str = ;postd=1pppp2;pause=2000ms
-
-#     #pushes the data to the server
-#     T.record(say="", \
-#         beep=False, \
-#         maxTime=8, \
-#         transcription= {"url": "http://autotapp.herokuapp.com/transcribe"}, \
-#         format='json'
-#         )
-
-#     sleep(3000)
-
-#     optionsCorpus = requests.get('https://autotapp.firebaseio.com/results.json').json()
-#     optionList = filter(None, [option.strip(" , or and").split()[1:] \
-#         for option in optionsCorpus.lower().split("press")])
 
 
     #somehow find out whether the 
