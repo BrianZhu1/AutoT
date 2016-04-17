d2d70e554bd15fc3672ddd1a254720c76d77eb0f
smerdy <joshua.pham@gmail.com>
2016-04-17 02:24:50 -0500

    testing transcribe route with dummy output through postman 3

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def hello():
 @app.route("/transcribe", methods=["POST"])
 def transcribe():
     headers = {'Content-Type': 'application/json'}
-    requests.put('https://autotapp.firebaseio.com/results.json', data=request.get_json(), headers=headers)
+    requests.put('https://autotapp.firebaseio.com/results.json', data=request.json, headers=headers)
     # posted = fb.post('/transcriptions', request.get_json())
     return redirect('/')
 
