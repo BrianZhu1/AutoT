7d5f8f929cf29792c3221d144afddcd979ce3a34
Brian Zhu <brian.zhu@utexas.edu>
2016-04-16 19:28:51 -0500

    can text to Brian using http get

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
+"""
 @app.route("/")
 def hello():
 	var callerID = currentCall.callerID;
@@ -18,6 +19,21 @@ def hello():
 @app.route("/transcribed_audio")
 def transcribed_audio:
 	return request.json
+
+"""
+
+@app.route("/")
+def hello():
+    my_token = "436c63746752724667426b4f4476635275426c614763666a5774724776504e4f6c4a41514c56544246527248"
+    my_num_to_dial = "16086952116"
+    my_name = "Brian+Zhu"
+    my_message = "the+sky+is+falling"
+    base_string = "https://api.tropo.com/1.0/sessions?action=create&"
+    http_string = base_string + "token=" + my_token + "&numberToDial=" + my_num_to_dial + "&customerName=" + my_name + "&msg=" + my_message
+    r = requests.get(http_string)
+    return "Hello from Python!"
+
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
